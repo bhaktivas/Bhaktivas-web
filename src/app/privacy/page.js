70738d8acaa9e01f59ee12a16a257324dc4bc4c8
crmranjeet-rgb <crmranjeet@gmail.com>
2026-07-16 18:24:41 +0530

    initial base code

---
+++ src/app/privacy/page.js
@@ -0,0 +1,9 @@
+export default function PrivacyPage() {
+  return (
+    <main className="min-h-screen p-10">
+      <h1 className="text-4xl font-bold">
+        Privacy Policy
+      </h1>
+    </main>
+  );
+}
---
+++ src/app/terms/page.js
@@ -0,0 +1,9 @@
+export default function TermsPage() {
+  return (
+    <main className="min-h-screen p-10">
+      <h1 className="text-4xl font-bold">
+        Terms and Conditions
+      </h1>
+    </main>
+  );
+}
